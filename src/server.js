d6309c5a931bedba6c1d8f39ad699c3166f46327
dongkam <84716824+dongkam5@users.noreply.github.com>
2022-01-30 16:19:18 +0900

    WebSocket으로 채팅 구현

---
+++ src/server.js
@@ -0,0 +1,40 @@
+import express from "express";
+import WebSocket, { WebSocketServer } from "ws";
+import path from "path";
+import http from 'http';
+
+const app = express();
+const __dirname=path.resolve();
+app.set('view engine','pug');
+app.set('views', __dirname+"/views");
+app.use('/public',express.static(__dirname+'/public'));
+app.get('/',(req,res)=> res.render('home'));
+app.get('/*',(req,res)=>res.redirect("/"));
+
+const handleListen= () => console.log(`Listening on http://localhost:3000`);
+
+const server=http.createServer(app);
+const wss= new WebSocketServer({ server });
+
+const sockets=[];
+wss.on('connection',(socket)=>{
+    sockets.push(socket);
+    socket["nickname"]="Anon";
+    console.log('Connected to Browser');
+    socket.on("close",()=>{
+        console.log("Disconnected from the Brower");
+    });
+    socket.on('message',(msg)=>{
+        const message=JSON.parse(msg);
+        switch(message.type){
+            case "new_message":
+                sockets.forEach((aSocket)=>aSocket.send(`${socket.nickname}: ${message.payload.toString('utf8')}`));
+                break;
+            case "nickname":
+                socket["nickname"]=message.payload;
+                break;
+        }
+    });
+});
+
+server.listen(3000, handleListen);
